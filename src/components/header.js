6880a44946b6844f61e9bca2cbea6ed728077592
Rajasekar T <kt.rajasekar@gmail.com>
2020-09-30 22:12:23 +0530

    Icons and Validations added

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,15 @@
 import React from "react";
-
+import mileageLogo from '../images/car-icon.svg';
 function Header() {
   return (
-    <div>
-      <h2 className="text-center">Mileage Calculator </h2>
-    </div>
+    <header>
+      <div>
+      <img src={mileageLogo} />
+      </div>
+      <div>
+        <h2 className="text-center">Mileage Calculator </h2>
+      </div>
+    </header>
   );
 }
 
